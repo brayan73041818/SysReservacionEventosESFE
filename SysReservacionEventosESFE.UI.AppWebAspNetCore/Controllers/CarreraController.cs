f5c5e724412ed325b20691792e9213398344fa30
Oscar Romero <ro23001@esfe.agape.edu.sv>
2024-01-24 17:43:54 -0600

    Carrera cambios

---
+++ SysReservacionEventosESFE.UI.AppWebAspNetCore/Controllers/CarreraController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SysReservacionEventosESFE.UI.AppWebAspNetCore.Controllers
+{
+    public class CarreraController : Controller
+    {
+        // GET: CarreraController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CarreraController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarreraController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarreraController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarreraController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CarreraController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarreraController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CarreraController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
