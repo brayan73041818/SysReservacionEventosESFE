e3bb4e839234461934e3ff747c0e0e9ae91da386
Oscar Romero <ro23001@esfe.agape.edu.sv>
2024-01-24 12:11:07 -0600

    Agregar archivos de proyecto.

---
+++ SysReservacionEventosESFE.UI.AppWebAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SysReservacionEventosESFE.UI.AppWebAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
