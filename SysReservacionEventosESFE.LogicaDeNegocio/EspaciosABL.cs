ea4c18a702d73aba025f7157c6c59ef5c65527e0
Jose-David-Montoya-Moreno <mm23003@esfe.agape.edu.sv>
2024-01-25 11:18:09 -0600

    Bl de espacios

--- SysReservacionEventosESFE.LogicaDeNegocio/EspaciosABL.cs
+++ SysReservacionEventosESFE.LogicaDeNegocio/EspaciosABL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SysReservacionEventosESFE.AccesoADatos;
+using SysReservacionEventosESFE.EntidadesDeNegocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace SysReservacionEventosESFE.LogicaDeNegocio
 {
-    internal class EspaciosABL
+    public class EspaciosABL
     {
+        public async Task<int> CrearAsync(EspaciosA pEspaciosA)
+        {
+            return await EspaciosADAL.CrearAsync(pEspaciosA);
+        }
+        public async Task<int> ModificarAsync(EspaciosA pEspaciosA)
+        {
+            return await EspaciosADAL.ModificarAsync(pEspaciosA);
+        }
+        public async Task<int> EliminarAsync(EspaciosA pEspaciosA)
+        {
+            return await EspaciosADAL.EliminarAsync(pEspaciosA);
+        }
+        public async Task<EspaciosA> ObtenerPorIdAsync(EspaciosA pEspaciosA)
+        {
+            return await EspaciosADAL.ObtenerPorIdAsync(pEspaciosA);
+        }
+        public async Task<List<EspaciosA>> ObtenerTodosAsync()
+        {
+            return await EspaciosADAL.ObtenerTodosAsync();
+        }
+        public async Task<List<EspaciosA>> BuscarAsync(EspaciosA pEspaciosA)
+        {
+            return await EspaciosADAL.BuscarAsync(pEspaciosA);
+        }
     }
 }
